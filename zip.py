2d4cc5d44d84ac4342fc6fbde88c19cea3c215b5
WhiteTiger-21 <https://twitter.com/WhiteTiger_21_s>
2021-08-19 21:19:40 +0900

    Add images to ZIP,but can't remove

--- zip.py
+++ zip.py
@@ -13,19 +13,32 @@ def img_convert(img_bin,img_name):
         MIN = min(width/1920,height/1920)
         img_resize = img.resize((int(width/MIN),int(height/MIN)),Image.LANCZOS)
         img_resize = img_resize.convert("RGB")
-        print("rm:"+img_name)
         #os.remove(img_name)
-        img_name = img_name[:-3]+"jpg"
-        img_resize.save(img_name,quality=90)
+        output = io.BytesIO()
+        img_resize.save(output,format="JPEG",quality=90)
+        return  output.getvalue()
         
 arg = sys.argv
-zf = zipfile.ZipFile(arg[1])
-lst = zf.namelist()
+if len(arg) == 1:
+    print("No ZIP file exist")
+    sys.exit()
+    
+zfi = zipfile.ZipFile(arg[1])
+lst = zfi.namelist()
+
 
 for img_name in lst:
     if img_name[-3:] == "png" or img_name[-3:] == "PNG":
-        img_file = zf.open(img_name)
+        img_file = zfi.open(img_name)
         img_bin = io.BytesIO(img_file.read())
         print(img_name)
-        \
-        img_convert(img_bin,img_name)
+        img_jpg=img_convert(img_bin,img_name)
+        img_name = img_name[:-3]+"jpg"
+        img_path = img_name.split("/")
+        #print(img_path[-1])
+        zfi.close()
+        with zipfile.ZipFile(arg[1],"a") as zfo:
+            zfo.write(img_path[-1])
+        #inf = zipfile.ZipInfo(img_path[-1])
+        #zfi.writestr(inf,img_jpg)
+        zfi = zipfile.ZipFile(arg[1])
